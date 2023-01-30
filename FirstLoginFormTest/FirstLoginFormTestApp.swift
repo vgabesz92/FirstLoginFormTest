2b2b3a2f45911488c0f5c882d02b59cd07eb5c7b
vgabesz92 <vgabesz@icloud.com>
2023-01-30 19:19:17 +0100

    Initial Commit

---
+++ FirstLoginFormTest/FirstLoginFormTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstLoginFormTestApp.swift
+//  FirstLoginFormTest
+//
+//  Created by Virág Gábor on 2023. 01. 30..
+//
+
+import SwiftUI
+
+@main
+struct FirstLoginFormTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
